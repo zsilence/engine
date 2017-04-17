8d1dfa8129ad747487fc8b499c9c107685bfc593
zz <tjzhangzhao@gmail.com>
2017-04-17 16:05:44 +0800

    begin

---
+++ engine.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#-----------------some variable----------------#
+L = 0.25
+nx = 41
+dx = L/nx
+nt = 25
+f = 50
+dt = 1/f
+
+#--------------------------------#
+
+
